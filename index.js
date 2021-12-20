e72719ea603e91b33c4c32ba6c9d8c2b0757a5c6
Théo Bezin <49711162+theobezin@users.noreply.github.com>
2021-12-20 12:40:57 +0000

    help command

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const Twisted = require("twisted")
 const LeagueJS = require("leaguejs")
 const leagueJs = new LeagueJS(process.env.LEAGUE_API_KEY)
 const prefix = ":"
-const tftapi = new Twisted.TftApi({key: `${process.env.LEAGUE_API_KEY}`})
+const tftapi = new Twisted.TftApi({ key: `${process.env.LEAGUE_API_KEY}` })
 
 process.env.LEAGUE_API_PLATFORM_ID = 'euw1'
 
 client.on("ready", () => {
+  client.user.setActivity(':help', { type: 'PLAYING' });
   console.log(`Logged in as ${client.user.tag}!`)
   console.log(client.user.displayAvatarURL())
 })
@@ -20,8 +21,19 @@ client.on("message", async (message) => {
 
   if (!message.content.startsWith(prefix)) return;
 
+  if (message.content.startsWith(`${prefix}help`)) {
+    const embed = new Discord.MessageEmbed();
+    embed.setColor("#216e00")
+    embed.setTitle("Commands list")
+    embed.addFields(
+      { name: 'help', value: 'Display the list of commands' },
+      { name: 'player', value: ':player player_name | Get global informations about a player' })
+    embed.setFooter(`${client.user.username} by Sadeuh`, client.user.displayAvatarURL());
+    message.channel.send(embed)
+  }
+
   if (message.content.startsWith(`${prefix}player`)) {
-    
+
     var name = ""
     console.log(name)
 
@@ -33,63 +45,64 @@ client.on("message", async (message) => {
     console.log(name)
 
     leagueJs.Summoner
-	    .gettingByName(name)
-	    .then(data => {
-		    'use strict';
+      .gettingByName(name)
+      .then(data => {
+        'use strict';
         console.log(data)
-        
+
         tftapi.League.get(data.id, process.env.LEAGUE_API_PLATFORM_ID)
           .then(league => {
-            
+
             console.log(league.response[0])
             var rank = "NC"
             var wins = "/"
-            if (typeof league.response[0] !== "undefined"){
+            if (typeof league.response[0] !== "undefined") {
               console.log("oui")
-              if (typeof league.response[0].tier !== "undefined"){
+              if (typeof league.response[0].tier !== "undefined") {
                 rank = league.response[0].tier + ' ' + league.response[0].rank + ' - ' + league.response[0].leaguePoints + ' LP'
               }
-              
-              
+
+
               wins = league.response[0].wins
             }
 
-            if(typeof data !== "undefined"){
+            if (typeof data !== "undefined") {
               const embed = new Discord.MessageEmbed();
-            embed.setColor("#216e00")
-            embed.setTitle(data.name)
-            embed.setThumbnail(`http://ddragon.leagueoflegends.com/cdn/10.15.1/img/profileicon/${data.profileIconId}.png`)
-            embed.addFields(
-              { name: 'Level', value: data.summonerLevel},
-              { name: 'Rank', value: rank},
-              { name: '\u200B', value: '\u200B' },
-              { name: 'Wins', value: wins, inline: true },
-              { name: '% of top 1', value: 'Soon...', inline: true })
-            embed.addField('% of top 4', 'Soon...', true)
-            embed.setFooter(`${client.user.username} by Sadeuh`, client.user.displayAvatarURL());
-            message.channel.send(embed)
-            }else{
-              
+              embed.setColor("#216e00")
+              embed.setTitle(data.name)
+              embed.setThumbnail(`http://ddragon.leagueoflegends.com/cdn/10.15.1/img/profileicon/${data.profileIconId}.png`)
+              embed.addFields(
+                { name: 'Level', value: data.summonerLevel },
+                { name: 'Rank', value: rank },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Wins', value: wins, inline: true },
+                { name: '% of top 1', value: 'Soon...', inline: true })
+              embed.addField('% of top 4', 'Soon...', true)
+              embed.setFooter(`${client.user.username} by Sadeuh`, client.user.displayAvatarURL());
+              message.channel.send(embed)
+            } else {
+
             }
-            
-          })        
-        
-        
 
-        
+          })
 
-        
-		    
+
+
+
+
+
+
       })
       .catch(err => {
         'use strict';
-		    console.log(err);
+        console.log(err);
         message.channel.send("Joueur non trouvé.")
-	});
+      });
   }
 
 })
 
 
 keepAlive()
+
 client.login(process.env.TOKEN)
